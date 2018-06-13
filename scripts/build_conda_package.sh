83c0f526b3114fbd73fcf811818bb03bcc081a1e
Andrew Higgins <ahiggins@palantir.com>
2018-06-13 17:43:07 +0100

    just use conda-build directly

--- scripts/build_conda_package.sh
+++ scripts/build_conda_package.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 
-if [ -z ${CONDA_BUILD+x} ]; then
-    echo "CONDA_BUILD is unset, set this to be the path to the conda-build command before running '$(basename "$0")'"
-    exit 1
-fi
-
 PACKAGE_VERSION=$(git describe --tags --always --first-parent)
 export PACKAGE_VERSION="${PACKAGE_VERSION//-/_}"
 
-$CONDA_BUILD --output-folder build/conda/ conda_recipe
+conda-build --output-folder build/conda/ conda_recipe
