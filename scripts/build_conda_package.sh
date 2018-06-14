0b5d470dac9a340bfea2fc63d3e970e3c2dcea0d
Andrew Higgins <ahiggins@palantir.com>
2018-06-14 11:44:19 +0100

    use conda variants for python 2.7 and 3.6

--- scripts/build_conda_package.sh
+++ scripts/build_conda_package.sh
@@ -3,4 +3,4 @@
 PACKAGE_VERSION=$(git describe --tags --always --first-parent)
 export PACKAGE_VERSION="${PACKAGE_VERSION//-/_}"
 
-conda-build --python 2.7 --output-folder build/conda/ conda_recipe
+conda-build --output-folder build/conda/ conda_recipe
