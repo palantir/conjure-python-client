3a45b58e6c06d9b32d0fc2e09d4b74d34146df3d
ahigginspalantir <ahiggins@palantir.com>
2018-06-18 16:26:37 +0100

    Add conda-build job to CI (#1)
    * conda packaging
    
    * attempt to use conda image
    
    * with CONDA_BUILD set
    
    * just use conda-build directly
    
    * explicitly bash
    
    * install conda-build
    
    * skip annotation for enum34 when on py34+
    
    * force python 2.7
    
    * only build conda on tags on develop
    
    * use conda variants for python 2.7 and 3.6
    
    * update github url
    
    * rename conda package to conda-lib to match wheel

---
+++ scripts/build_conda_package.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+PACKAGE_VERSION=$(git describe --tags --always --first-parent)
+export PACKAGE_VERSION="${PACKAGE_VERSION//-/_}"
+
+conda-build --output-folder build/conda/ conda_recipe
