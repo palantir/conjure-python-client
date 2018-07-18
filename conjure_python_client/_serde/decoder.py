cfafd77702a5e4f52c1d2456f733c963455438f7
Felipe Orozco <ferozco@users.noreply.github.com>
2018-07-18 22:24:33 +0100

    Deserialize optionals (#10)
    * Use snake_case arg names in optional deserialization
    
    * fix
    
    * addressed comments

--- conjure_python_client/_serde/decoder.py
+++ conjure_python_client/_serde/decoder.py
@@ -43,30 +43,33 @@ class ConjureDecoder(object):
             A instance of a bean of type conjure_type.
         """
         deserialized = {}  # type: Dict[str, Any]
-        for (arg_name, field_definition) in conjure_type._fields().items():
+        for (python_arg_name, field_definition) \
+                in conjure_type._fields().items():
             field_identifier = field_definition.identifier
 
             if field_identifier not in obj or obj[field_identifier] is None:
-                cls.check_null_field(obj, deserialized, field_definition)
+                cls.check_null_field(
+                    obj, deserialized, python_arg_name, field_definition)
             else:
                 value = obj[field_identifier]
                 field_type = field_definition.field_type
-                deserialized[arg_name] = cls.do_decode(value, field_type)
+                deserialized[python_arg_name] = \
+                    cls.do_decode(value, field_type)
         return conjure_type(**deserialized)
 
     @classmethod
-    def check_null_field(cls, obj, deserialized, field_definition):
-        field_identifier = field_definition.identifier
+    def check_null_field(
+            cls, obj, deserialized, python_arg_name, field_definition):
         if isinstance(field_definition.field_type, ListType):
-            deserialized[field_identifier] = []
+            deserialized[python_arg_name] = []
         elif isinstance(field_definition.field_type, DictType):
-            deserialized[field_identifier] = {}
+            deserialized[python_arg_name] = {}
         elif isinstance(field_definition.field_type, OptionalType):
-            deserialized[field_identifier] = None
+            deserialized[python_arg_name] = None
         else:
             raise Exception(
                 "field {} not found in object {}".format(
-                    field_identifier, obj
+                    field_definition.identifier, obj
                 )
             )
 
--- scripts/download_conjure_python.sh
+++ scripts/download_conjure_python.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-set -e
+set -ex
 
-VERSION=3.2.0
+VERSION=0.1.2
 ARTIFACT_NAME="conjure-python-${VERSION}"
 DOWNLOAD_OUTPUT="build/downloads/conjure-python.tgz"
 
 mkdir -p build/downloads
-curl "https://palantir.bintray.com/releases/com/palantir/conjure/python/conjure-python/${VERSION}/${ARTIFACT_NAME}.tgz" -o "$DOWNLOAD_OUTPUT"
+curl -L "https://palantir.bintray.com/releases/com/palantir/conjure/python/conjure-python/${VERSION}/${ARTIFACT_NAME}.tgz" -o "$DOWNLOAD_OUTPUT"
 
 tar xf "$DOWNLOAD_OUTPUT" -C build
 mv -f "build/$ARTIFACT_NAME" build/conjure-python
