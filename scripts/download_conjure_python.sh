aad2b7f8266691341a979033a4d43eaf0395247e
Dan Sănduleac <dansanduleac@users.noreply.github.com>
2018-07-19 13:25:32 +0100

    Bump verifier to 0.5.0, generator to 3.3.0 (#11)

--- scripts/download_conjure_python.sh
+++ scripts/download_conjure_python.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-VERSION=0.1.2
+VERSION=3.3.0
 ARTIFACT_NAME="conjure-python-${VERSION}"
 DOWNLOAD_OUTPUT="build/downloads/conjure-python.tgz"
 
@@ -10,4 +10,5 @@ mkdir -p build/downloads
 curl -L "https://palantir.bintray.com/releases/com/palantir/conjure/python/conjure-python/${VERSION}/${ARTIFACT_NAME}.tgz" -o "$DOWNLOAD_OUTPUT"
 
 tar xf "$DOWNLOAD_OUTPUT" -C build
+rm -rf "build/conjure-python"
 mv -f "build/$ARTIFACT_NAME" build/conjure-python
--- scripts/generate_test_bindings.sh
+++ scripts/generate_test_bindings.sh
@@ -6,17 +6,3 @@ CONJURE_PYTHON="build/conjure-python/bin/conjure-python"
 
 $CONJURE_PYTHON generate build/resources/verification-api.conjure.json test --packageVersion 0.0.0 --packageName generated
 rm test/setup.py
-
-# HACKHACK to handle the package name change without needing the new generator
-GENERATED_DIR=test/generated/conjure_verification
-case $(uname -s) in
-    Linux*)
-        find $GENERATED_DIR -name "*.py" -type f -exec sed -i 's/from conjure/from conjure_python_client/g' {} \;
-        find $GENERATED_DIR -name "*.py" -type f -exec sed -i 's/from httpremoting/from conjure_python_client/g' {} \;
-    ;;
-    Darwin*)
-        find $GENERATED_DIR -name "*.py" -type f -exec sed -i '' 's/from conjure/from conjure_python_client/g' {} \;
-        find $GENERATED_DIR -name "*.py" -type f -exec sed -i '' 's/from httpremoting/from conjure_python_client/g' {} \;
-    ;;
-    *) echo "Unsupported OS" >&2; exit 1;;
-esac
