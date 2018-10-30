f23bac4e97a7d8eeaf23f784458ee49875f1b3bc
Dan Sanduleac <dsanduleac@palantir.com>
2018-10-30 14:35:38 +0000

    fix script

--- scripts/generate_test_bindings.sh
+++ scripts/generate_test_bindings.sh
@@ -4,5 +4,5 @@ set -e
 
 CONJURE_PYTHON="build/conjure-python/bin/conjure-python"
 
-$CONJURE_PYTHON generate build/resources/verification-api.conjure.json test --packageVersion 0.0.0 --packageName generated
+$CONJURE_PYTHON generate build/resources/verification-server-api.conjure.json test --packageVersion 0.0.0 --packageName generated
 rm test/setup.py
