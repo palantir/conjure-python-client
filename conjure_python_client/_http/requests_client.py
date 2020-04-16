cf0cdcfb0f4379804c40e639bdb0683686288780
Benjamin Duffield <bduffield@palantir.com>
2020-04-16 15:33:42 -0400

    don't include first three

--- conjure_python_client/_http/requests_client.py
+++ conjure_python_client/_http/requests_client.py
@@ -31,9 +31,10 @@ T = TypeVar("T")
 # https://testssl.sh/openssl-rfc.mappping.html
 # https://wiki.mozilla.org/Security/Server_Side_TLS#Intermediate_compatibility_.28recommended.29
 CIPHERS = (
-    "TLS_AES_128_GCM_SHA256:"
-    "TLS_AES_256_GCM_SHA384:"
-    "TLS_CHACHA20_POLY1305_SHA256:"
+    # don't include these first three
+    # "TLS_AES_128_GCM_SHA256:"
+    # "TLS_AES_256_GCM_SHA384:"
+    # "TLS_CHACHA20_POLY1305_SHA256:"
     "ECDHE-ECDSA-AES128-GCM-SHA256:"
     "ECDHE-RSA-AES128-GCM-SHA256:"
     "ECDHE-ECDSA-AES256-GCM-SHA384:"
