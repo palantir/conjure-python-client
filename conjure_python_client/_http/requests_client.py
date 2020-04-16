fa101bd9e7f01c49b030ea19283bdae35b5cea23
Benjamin Duffield <bduffield@palantir.com>
2020-04-16 16:00:53 -0400

    bring back tls 1.3 with comment

--- conjure_python_client/_http/requests_client.py
+++ conjure_python_client/_http/requests_client.py
@@ -31,10 +31,12 @@ T = TypeVar("T")
 # https://testssl.sh/openssl-rfc.mappping.html
 # https://wiki.mozilla.org/Security/Server_Side_TLS#Intermediate_compatibility_.28recommended.29
 CIPHERS = (
-    # don't include the tls 1.3 ones
-    # "TLS_AES_128_GCM_SHA256:"
-    # "TLS_AES_256_GCM_SHA384:"
-    # "TLS_CHACHA20_POLY1305_SHA256:"
+    # tls 1.3 ciphers
+    # n.b. this may not actually be honoured given https://docs.python.org/3/library/ssl.html#ssl.SSLContext.set_ciphers
+    "TLS_AES_128_GCM_SHA256:"
+    "TLS_AES_256_GCM_SHA384:"
+    "TLS_CHACHA20_POLY1305_SHA256:"
+    # non-tls 1.3 ciphers
     "ECDHE-ECDSA-AES128-GCM-SHA256:"
     "ECDHE-RSA-AES128-GCM-SHA256:"
     "ECDHE-ECDSA-AES256-GCM-SHA384:"
