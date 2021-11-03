c25d7347a23991e23cf85e74490aee92e36b635c
Jacek Lach <jlach@palantir.com>
2021-11-03 14:57:04 +0000

    Fix download urls in ETE

--- scripts/download_conjure_python.sh
+++ scripts/download_conjure_python.sh
@@ -7,7 +7,7 @@ ARTIFACT_NAME="conjure-python-${VERSION}"
 DOWNLOAD_OUTPUT="build/downloads/conjure-python.tgz"
 
 mkdir -p build/downloads
-curl -L "https://palantir.bintray.com/releases/com/palantir/conjure/python/conjure-python/${VERSION}/${ARTIFACT_NAME}.tgz" -o "$DOWNLOAD_OUTPUT"
+curl -L "https://search.maven.org/remotecontent?filepath=com/palantir/conjure/python/conjure-python/${VERSION}/${ARTIFACT_NAME}.tgz" -o "$DOWNLOAD_OUTPUT"
 
 tar xf "$DOWNLOAD_OUTPUT" -C build
 rm -rf "build/conjure-python"
